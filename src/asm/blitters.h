fc76c358efd7323cbfda88059bc24896cd9f33d7
Eli Dayan <eli.dayan@gmail.com>
2024-06-19 18:48:05 -0400

    revert blitters

--- src/asm/blitters.h
+++ src/asm/blitters.h
@@ -11,7 +11,14 @@ void blit_windowed_dirty_mmx (uint8 *src,
 							  uint8 *dst, 
 							  size_t size, 
 							  int32 bpp);
-							 
+							  
+void blit_2x_mmx (uint8 *dest, uint8 *source, size_t size);
+
+void blit_2x_dirty_mmx (uint8 *dest, 
+						uint8 *source, 
+						uint8 *dirty, 
+						int32 rowbytes, 
+						size_t size);
 
 void blit_2x_windowed_dirty_mmx(uint8 *src, 
 								uint8 *dirty, 
@@ -19,14 +26,6 @@ void blit_2x_windowed_dirty_mmx(uint8 *src,
 								size_t size, 				
 								int32 bpp, 
 								int32 rowbytes);
-								
-void blit_2x_dirty_mmx (uint8 *dest, 
-						uint8 *source, 
-						uint8 *dirty, 
-						int32 rowbytes, 
-						size_t size);
-								
-
 
 void blit_overlay (uint8 *dest, 
 				   uint8 *source, 
