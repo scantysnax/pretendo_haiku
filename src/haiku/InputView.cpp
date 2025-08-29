bb7f98f691455cf70dd6250de4b94f1cd7a4a9df
Eli Dayan <eli.dayan@gmail.com>
2025-08-28 21:27:17 -0400

    whoops removed some unneeded font code

--- src/haiku/InputView.cpp
+++ src/haiku/InputView.cpp
@@ -26,11 +26,7 @@ InputView::AttachedToWindow()
 	r.Set(32, 400, 80, 420);
 	fUpTextView = new BTextView (r, "up_view", 
 		BRect(3, 3, r.Width() - 3, r.Height() - 3), B_FOLLOW_LEFT | B_FOLLOW_TOP, 0);
-	
-	BFont f = be_plain_font;
-	f.SetSize(11.0f);
-	fUpTextView->SetViewColor(ui_color(B_PANEL_BACKGROUND_COLOR));
-	fUpTextView->SetFontAndColor(&f);		
+			
 	AddChild(fUpTextView);
 }
 
