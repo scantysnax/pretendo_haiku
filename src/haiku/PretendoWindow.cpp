de763278a33a03c16f12765378a4ee3cf0fd4f3e
eli <eli.dayan@gmail.com>
2025-09-17 14:56:59 -0400

    show palette window

--- src/haiku/PretendoWindow.cpp
+++ src/haiku/PretendoWindow.cpp
@@ -690,10 +690,10 @@ PretendoWindow::OnAdjustPalette()
 {
 	puts(__PRETTY_FUNCTION__);
 	
-#if 0
+//#if 0
 	if (fPaletteWindow && fPaletteWindow->Lock()) {
-		//fPaletteWindow->Quit();
-		//fPaletteWindow = nullptr;
+		fPaletteWindow->Quit();
+		fPaletteWindow = nullptr;
 		fPaletteWindow->Show();
 	} else {
 		fPaletteWindow = new PaletteWindow(this);
@@ -711,7 +711,7 @@ PretendoWindow::OnAdjustPalette()
 	}
 	
 	fPaletteWindow->Show();
-#endif
+//#endif
 }
 
 void
