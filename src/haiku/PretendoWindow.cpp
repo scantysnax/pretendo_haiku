b9f976855b6eabe28f568c7b9cef49ca4f92ac95
eli <eli.dayan@gmail.com>
2025-09-30 14:33:16 -0400

    change order of menu

--- src/haiku/PretendoWindow.cpp
+++ src/haiku/PretendoWindow.cpp
@@ -540,7 +540,7 @@ PretendoWindow::AddMenu()
 	fVideoMenu->AddItem(new BMenuItem("DirectWindow", new BMessage(MSG_CHANGE_RENDER)));
 	fVideoMenu->AddItem(new BMenuItem("WindowScreen", new BMessage(MSG_CHANGE_RENDER), 'F'));
 	fVideoMenu->SetRadioMode(true);
-	fEmuMenu->AddItem(new BMenuItem("Input" B_UTF8_ELLIPSIS, new BMessage(MSG_CFG_INPUT)));
+	//fEmuMenu->AddItem(new BMenuItem("Input" B_UTF8_ELLIPSIS, new BMessage(MSG_CFG_INPUT)));
 	
 	fAudioMenu = new BMenu("Audio");
 	fEmuMenu->AddItem(fAudioMenu);
@@ -555,6 +555,8 @@ PretendoWindow::AddMenu()
 	(fAudioMenu->ItemAt(2))->SetMarked(true);
 	(fAudioMenu->ItemAt(3))->SetMarked(true);
 	(fAudioMenu->ItemAt(4))->SetMarked(true);
+	
+	fEmuMenu->AddItem(new BMenuItem("Input" B_UTF8_ELLIPSIS, new BMessage(MSG_CFG_INPUT)));
 	
 	
 	fToolMenu->AddItem(new BMenuItem("Adjust Palette" B_UTF8_ELLIPSIS, new BMessage(MSG_ADJ_PALETTE)));
